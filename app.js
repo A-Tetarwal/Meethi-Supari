2c577b3c22a29df28f9b25632b61d598d5be3b30
A-Tetarwal <andashishwill@gmail.com>
2024-04-15 23:40:46 +0530

    updates

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const path = require("path");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
@@ -14,10 +15,18 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
-  });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup.ejs");
+});
 
 // Start the server
-const PORT = 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
